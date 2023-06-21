404b4ab188f602e98e67c510ff455d8e41e91c44
Thithi-Ux <morrinthithi2@gmail.com>
2023-06-21 22:06:43 +0300

    My first git project

---
+++ Intro to Data Analysis using Tidyverse.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+url <- "https://dax-cdn.cdn.appdomain.cloud/dax-airline/1.0.1/lax_to_jfk.tar.gz"
+
+download.file(url, destfile = "lax_to_jfk.tar.gz")
+
+untar("lax_to_jfk.tar.gz", tar = "internal")
+
+sub_airline <- read_csv("lax_to_jfk/lax_to_jfk.csv",
+                        col_types = cols(
+                        'DivDistance' = col_number(),
+                        'DivArrDelay' = col_number()
+                        ))
+
+tail(sub_airline, 5)
+
+dim(sub_airline)
+
+write_csv(sub_airline, "lax_to_jfk.csv")
---
+++ git add.R
@@ -0,0 +1,7 @@
+
+git config --global user.email "morrinthithi2@gmail.com"
+git config --global user.name "Thithi-Ux"
+
+
+
+
